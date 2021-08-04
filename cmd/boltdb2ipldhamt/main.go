d85342641094a46b1bbdbf4a82c37b8c722173fe
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2021-08-04 19:38:11 -0300

    Creating command line tool

--- cmd/boltdb2ipldhamt/main.go
+++ cmd/boltdb2ipldhamt/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "boltdb2ipldhamt",
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/emirpasic/gods v1.12.0
 	github.com/simplecoincom/go-ipld-adl-hamt-container v0.0.0-20210804180046-b7dbcc95b7e2
+	github.com/spf13/cobra v1.2.1
 	go.etcd.io/bbolt v1.3.6
 )
